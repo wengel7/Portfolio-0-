802c4a96b79ae3d862e4932bce2c9df62a79233f
Wengel <112881230+wengel7@users.noreply.github.com>
2023-06-23 18:26:16 +0300

    cancel the animation image

--- js/pic.js
+++ js/pic.js
@@ -1,37 +1,37 @@
-const images = document.getElementsByClassName("image");
+// const images = document.getElementsByClassName("image");
 
-let globalIndex = 0,
-    last = { x: 0, y: 0 };
+// let globalIndex = 0,
+//     last = { x: 0, y: 0 };
 
-const activate = (image, x, y) => {
-  image.style.left = `${x}px`;
-  image.style.top = `${y}px`;
-  image.style.zIndex = globalIndex;
-  image.dataset.status = "active";
+// const activate = (image, x, y) => {
+//   image.style.left = `${x}px`;
+//   image.style.top = `${y}px`;
+//   image.style.zIndex = globalIndex;
+//   image.dataset.status = "active";
 
-  last = { x, y };
-}
+//   last = { x, y };
+// }
 
-const distanceFromLast = (x, y) => {
-  return Math.hypot(x - last.x, y - last.y);
-}
+// const distanceFromLast = (x, y) => {
+//   return Math.hypot(x - last.x, y - last.y);
+// }
 
-const handleOnMove = e => {
-  if(distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / 20)) {
-    const lead = images[globalIndex % images.length],
-          tail = images[(globalIndex - 5) % images.length];
+// const handleOnMove = e => {
+//   if(distanceFromLast(e.clientX, e.clientY) > (window.innerWidth / 20)) {
+//     const lead = images[globalIndex % images.length],
+//           tail = images[(globalIndex - 5) % images.length];
 
-    activate(lead, e.clientX, e.clientY);
+//     activate(lead, e.clientX, e.clientY);
 
-    if(tail) tail.dataset.status = "inactive";
-    globalIndex++;
-    if(globalIndex == 6){
-      globalIndex = 0;
-    }
-  }
-}
+//     if(tail) tail.dataset.status = "inactive";
+//     globalIndex++;
+//     if(globalIndex == 6){
+//       globalIndex = 0;
+//     }
+//   }
+// }
 
-window.onmousemove = e => handleOnMove(e);
+// window.onmousemove = e => handleOnMove(e);
 
-window.ontouchmove = e => handleOnMove(e.touches[0]);
+// window.ontouchmove = e => handleOnMove(e.touches[0]);
 
